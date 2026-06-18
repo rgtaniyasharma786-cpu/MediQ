d93e70505239399491b8075cdaaf892540450a47
rgtaniyasharma786-cpu <rgtaniyasharma786@gmail.com>
2026-06-18 17:09:18 +0530

    change path

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
+  withCredentials: true
 });
 
 // Request interceptor - add auth token
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,23 +4,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-  server: {
-    port: 5173,
-    // Serve index.html for all routes so React Router handles navigation
-    // This ensures /login, /register, /admin etc. all work on refresh
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-      },
-      '/socket.io': {
-        target: process.env.VITE_SOCKET_URL,
-        changeOrigin: true,
-        ws: true,
-      },
-    },
-  },
+  // server: {
+  //   port: 5173,
+  //   // Serve index.html for all routes so React Router handles navigation
+  //   // This ensures /login, /register, /admin etc. all work on refresh
+  //   historyApiFallback: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.VITE_API_URL || 'http://localhost:5000',
+  //       changeOrigin: true,
+  //     },
+  //     '/socket.io': {
+  //       target: process.env.VITE_SOCKET_URL || 'http://localhost:5000',
+  //       changeOrigin: true,
+  //       ws: true,
+  //     },
+  //   },
+  // },
   preview: {
     port: 4173,
     // Also needed for production preview
@@ -30,19 +30,23 @@ export default defineConfig(({ mode }) => {
     outDir: 'dist',
     rollupOptions: {
       output: {
-        manualChunks(id) {
-        if (id.includes('react')) {
-          return 'vendor'
-        }
+       manualChunks(id) {
+  if (
+    id.includes('react') ||
+    id.includes('react-dom') ||
+    id.includes('react-router-dom')
+  ) {
+    return 'vendor'
+  }
 
-        if (id.includes('recharts')) {
-          return 'charts'
-        }
+  if (id.includes('recharts')) {
+    return 'charts'
+  }
 
-        if (id.includes('socket.io-client')) {
-          return 'socket'
-        }
-      },
+  if (id.includes('socket.io-client')) {
+    return 'socket'
+  }
+}
       },
     },
   },
